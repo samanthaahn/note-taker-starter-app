f3f6e208d5ed69aef2bea679a56b4d3aee3cd892
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-04-05 13:36:06 -0700

    added some basic code to serve.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = 3001; 
+const app  = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
