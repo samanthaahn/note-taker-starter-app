90e3173d767d4073ef02d26fe55eaee9f6160556
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-04-05 13:41:29 -0700

    added comments to my server.js

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+// Import express.js
 const express = require('express');
+// Import built-in node.js package 'path' to resolve path of files that are located on the server.
 const path = require('path');
-
+// This specifies which port the express.js will run on. 
 const PORT = 3001; 
+// Initialize an instance of express.js
 const app  = express();
 
+// Middleware for parsing applicatoin/json and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+// Static middleware pointing to the public folder
 app.use(express.static('public'));
+
+//Create Express.js routes for defualt '/' 
+app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
 
 
