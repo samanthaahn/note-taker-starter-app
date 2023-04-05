b5a26d42a01db4b65f1e1d61537ebe545b44ea79
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-04-05 13:37:32 -0700

    added the listenng funciton at the end of code

--- server.js
+++ server.js
@@ -7,3 +7,15 @@ const app  = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+console.log(`App listening at http://localhost:${PORT}`)
+);
